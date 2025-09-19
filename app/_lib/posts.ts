859803c7dc114d31e2114b100234a86d05c61cd8
obertobr <real.azzi@gmail.com>
2025-09-19 19:00:23 -0300

    feat: add tags to posts in db

--- app/_lib/posts.ts
+++ app/_lib/posts.ts
@@ -2,9 +2,15 @@ import prisma from "./prisma";
 
 export async function getPosts() {
     return await prisma.post.findMany({
-        orderBy: { createdAt: 'desc'}
+        orderBy: { createdAt: 'desc'},
+        include: {tags: true}
     });
 }
+
+export async function getTags() {
+    return await prisma.tag.findMany();
+}
+
 
 export async function getPostBySlug(slug: string) {
     return await prisma.post.findUnique({
@@ -12,10 +18,20 @@ export async function getPostBySlug(slug: string) {
     })
 }
 
-export async function createPost(title:string, content: string, slug: string) {
+export async function createPost(title:string, content: string, slug: string, tags: string[]) {
     try{
         return await prisma.post.create({
-            data: {title, content, slug}
+            data: {
+                title, 
+                content, 
+                slug,
+                tags: {
+                    connectOrCreate: tags.map((tagName) => ({
+                        where: {name: tagName},
+                        create: {name: tagName}
+                    }))
+                }
+            }
         })
     } catch ( err: any ){
         if(err.code === "P2002"){
